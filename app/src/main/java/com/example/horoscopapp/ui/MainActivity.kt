9c5eb35e123380587ebf365bdf1ac1f535ac80f2
User <>
2023-03-23 12:18:15 -0400

    Initial Commit

--- app/src/main/java/com/example/horoscopapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/horoscopapp/ui/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.horoscopapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscopapp.R
+import com.example.horoscopapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
+        val buttomNavigationView = findViewById<BottomNavigationView>(R.id.buttomNavView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        buttomNavigationView.setupWithNavController(navController = navController)
+
+
+
+
 
     }
 }
